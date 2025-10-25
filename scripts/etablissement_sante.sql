b827bba8e5787b7c944eed1b3aff05cff5c0cb10
noiia <edwin.lecomte31@gmail.com>
2025-10-25 13:17:55 +0200

    feat(imp) adding etablissement_sante insert, adding src sort to only keep unique rows

--- scripts/etablissement_sante.sql
+++ scripts/etablissement_sante.sql
@@ -13,7 +13,7 @@ SELECT
         to_utf8(concat_ws('|',
             coalesce(a.voie, ''),
             coalesce(a.numero_voie, ''),
-            coalesce('', ''),
+            '',
             coalesce(a.commune, ''),
             coalesce(a.code_postal, ''),
             coalesce(a.type_voie, ''),
@@ -27,7 +27,16 @@ SELECT
     a.code_postal,
     a.type_voie,
     a.indice_repetition_voie
-FROM datalake."public".etablissement_sante a
+FROM (
+    SELECT DISTINCT
+        voie,
+        numero_voie,
+        commune,
+        code_postal,
+        type_voie,
+        indice_repetition_voie
+    FROM datalake."public".etablissement_sante
+) a
 WHERE NOT EXISTS (
     SELECT 1
     FROM datawarehouse."default".adresse_dim w
@@ -55,13 +64,68 @@ WHERE NOT EXISTS (
 );
 
 
+
 INSERT INTO datawarehouse."default".pays_dim (id, nom)
 SELECT
-    xxhash64(to_utf8(concat_ws('|', coalesce(a.pays, '')))) AS id,
-    a.pays AS nom
-FROM datalake."public".etablissement_sante a
+    xxhash64(to_utf8(nom_pays)) AS id,
+    nom_pays AS nom
+FROM (
+    SELECT DISTINCT a.pays AS nom_pays
+    FROM datalake."public".etablissement_sante a
+) src
 WHERE NOT EXISTS (
     SELECT 1
     FROM datawarehouse."default".pays_dim p
-    WHERE p.nom = a.pays
+    WHERE p.nom = src.nom_pays
 );
+
+
+
+
+INSERT INTO datawarehouse."default".etablissement_sante (id, raison_sociale_site, id_adresse, id_pays)
+SELECT
+  a.identifiant_organisation,
+  a.raison_sociale_site,
+    xxhash64(
+        to_utf8(concat_ws('|',
+            coalesce(a.voie, ''),
+            coalesce(a.numero_voie, ''),
+            '',
+            coalesce(a.commune, ''),
+            coalesce(a.code_postal, ''),
+            coalesce(a.type_voie, ''),
+            coalesce(a.indice_repetition_voie, '')
+        ))
+    ) AS id_adresse,
+    xxhash64(to_utf8(coalesce(a.pays, ''))) AS id_pays
+FROM (
+    SELECT DISTINCT
+        identifiant_organisation,
+        raison_sociale_site,
+        voie,
+        numero_voie,
+        commune,
+        code_postal,
+        type_voie,
+        indice_repetition_voie,
+        pays
+    FROM datalake."public".etablissement_sante
+) a
+WHERE NOT EXISTS (
+    SELECT 1
+    FROM datawarehouse."default".etablissement_sante e
+    WHERE e.raison_sociale_site = a.raison_sociale_site
+      AND e.id_adresse = xxhash64(
+          to_utf8(concat_ws('|',
+              coalesce(a.voie, ''),
+              coalesce(a.numero_voie, ''),
+              '',
+              coalesce(a.commune, ''),
+              coalesce(a.code_postal, ''),
+              coalesce(a.type_voie, ''),
+              coalesce(a.indice_repetition_voie, '')
+          ))
+      )
+      AND e.id_pays = xxhash64(to_utf8(coalesce(a.pays, '')))
+);
+
